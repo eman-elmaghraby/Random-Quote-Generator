e27787728aa4b51b5b202cdc96c3452e08d852e7
eman amin <emanelmaghraby447@gmail.com>
2023-12-10 20:17:51 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,55 @@
+var quoteText= document.getElementById("quoteText");
+var quoteAuther = document.getElementById("quoteAuther");
+
+var quotes = [
+    {
+        quote:  "Be yourself; everyone else is already taken.",
+        auther: "Oscar Wilde"
+    },
+    {
+        quote:  "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best",
+        auther: "Oscar Wilde"
+    },
+     {
+        quote:  "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        auther: "Albert Einstein"
+    },
+       {
+        quote:  "So many books, so little time.",
+        auther: "Frank Zappa"
+    },
+        {
+        quote:  "A room without books is like a body without a soul.",
+        auther: "Marcus Tullius Cicero"
+    },
+    {
+        quote: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+        auther: "Bernard M. Baruch"
+    },
+    {
+        quote: "You've gotta dance like there's nobody watching,Love like you'll never be hurt,Sing like there's nobody listening, And live like it's heaven on earth",
+        auther: "William W. Purkey"
+    },
+    {
+        quote: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+        auther: "Dr. Seuss"
+    },
+    {
+        quote: "You only live once, but if you do it right, once is enough",
+        auther: "Mae West"
+    },
+    {
+        quote: "In three words I can sum up everything I've learned about life: it goes on.",
+        auther: " Robert Frost"
+    },
+]
+
+function getRandomQuote(){
+    var quote = Math.floor(Math.random() * quotes.length);
+    for(i=0; i<quotes.length; i++){
+        quoteText.innerHTML= quotes[quote].quote;
+        quoteAuther.innerHTML = quotes[quote].auther;
+        
+    }
+ 
+}
